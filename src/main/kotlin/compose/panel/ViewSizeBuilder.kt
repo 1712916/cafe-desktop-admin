988907e8c7b88180c0386a792a2355ee6eae903b
vinhnt <kieuphong2840@gmail.com>
2024-06-29 19:10:25 +0700

    fix view size builder

--- src/main/kotlin/compose/panel/ViewSizeBuilder.kt
+++ src/main/kotlin/compose/panel/ViewSizeBuilder.kt
@@ -1,26 +1,17 @@
 package compose.panel
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
 
 @Composable
 fun ViewSizeBuilder(builder: @Composable (viewSize: ViewSize) -> Unit) {
+
     var size by remember { mutableStateOf(IntSize.Zero) }
     val viewSize by remember { derivedStateOf { ViewSize.fromWindowSize(size) } }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-            .onSizeChanged { newSize ->
-                size = newSize
-            }
-    ) {
+    BoxWithConstraints {
+        size = IntSize(constraints.maxWidth, constraints.maxHeight)
         builder(viewSize)
     }
 }
